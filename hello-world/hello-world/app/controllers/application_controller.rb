f2f2328163340587554d815f1e68997b5df85bda
丸田遼 <marutaryou@marutaryous-MacBook-Air.local>
2018-12-22 17:50:08 +0900

    updated gemfile to install pg on production env

--- hello-world/hello-world/app/controllers/application_controller.rb
+++ hello-world/hello-world/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def hello 
+        render html: "¡hora, mundo!" 
+    end 
+    def goodbye 
+        render html: "¡adios, mundo!" 
+    end 
 end
